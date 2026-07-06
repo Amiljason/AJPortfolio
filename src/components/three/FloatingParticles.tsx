12ef98d0468ceafcc86e30645dc9793012145504
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-07-06 18:20:13 +0000

    Add floating particle system

--- src/components/three/FloatingParticles.tsx
+++ src/components/three/FloatingParticles.tsx
@@ -1,5 +1,22 @@
+import { Points, PointMaterial } from "@react-three/drei";
+
 function FloatingParticles() {
-  return null;
+  const positions = new Float32Array(500 * 3);
+
+  for (let i = 0; i < positions.length; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+  }
+
+  return (
+    <Points positions={positions} stride={3}>
+      <PointMaterial
+        transparent
+        size={0.03}
+        sizeAttenuation
+        depthWrite={false}
+      />
+    </Points>
+  );
 }
 
 export default FloatingParticles;
