8fa0c076b103cdee882cbe8e929bfb33867206aa
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-07-06 17:58:30 +0000

    Move about content into data file

---
+++ src/data/about.ts
@@ -0,0 +1,6 @@
+export const about = {
+  title: "About Me",
+  subtitle: "Get to know me better.",
+  description:
+    "I'm a Computer Science undergraduate passionate about software engineering, full-stack web development, and solving complex algorithmic problems. I enjoy building modern, scalable applications while continuously exploring new technologies and improving my problem-solving skills.",
+};
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,6 @@
 import Container from "../components/Container";
 import SectionHeading from "../components/SectionHeading";
+import { about } from "../data/about";
 
 function About() {
   return (
@@ -9,16 +10,12 @@ function About() {
     >
       <Container>
         <SectionHeading
-          title="About Me"
-          subtitle="Get to know me better."
+          title={about.title}
+          subtitle={about.subtitle}
         />
 
         <p className="mx-auto max-w-3xl text-center text-lg leading-8 text-slate-400">
-          I'm a Computer Science undergraduate passionate about software
-          engineering, full-stack web development, and solving complex
-          algorithmic problems. I enjoy building modern, scalable applications
-          while continuously exploring new technologies and improving my
-          problem-solving skills.
+          {about.description}
         </p>
       </Container>
     </section>
