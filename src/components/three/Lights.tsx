06086b9afd2685fe5d1aa6195daaab57d3bce4d5
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-07-06 18:21:31 +0000

    Improve 3D scene lighting

--- src/components/three/Lights.tsx
+++ src/components/three/Lights.tsx
@@ -1,16 +1,23 @@
 function Lights() {
   return (
     <>
-      <ambientLight intensity={0.6} />
+      <ambientLight intensity={0.35} />
 
       <directionalLight
         position={[5, 5, 5]}
-        intensity={2}
+        intensity={2.5}
       />
 
       <pointLight
-        position={[-5, -5, 5]}
-        intensity={1}
+        position={[-4, 2, 4]}
+        intensity={2}
+        color="#60a5fa"
+      />
+
+      <pointLight
+        position={[4, -2, 2]}
+        intensity={1.5}
+        color="#a855f7"
       />
     </>
   );
