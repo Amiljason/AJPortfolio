2a7baed881139c2718b0082daf1b7547f821f83a
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-07-06 17:49:46 +0000

    Refactor skills section to use container

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -1,3 +1,4 @@
+import Container from "../components/Container";
 import SectionHeading from "../components/SectionHeading";
 import { skills } from "../data/skills";
 
@@ -5,9 +6,9 @@ function Skills() {
   return (
     <section
       id="skills"
-      className="bg-slate-900 px-6 py-28"
+      className="bg-slate-900 py-28"
     >
-      <div className="mx-auto max-w-6xl">
+      <Container>
         <SectionHeading
           title="Skills"
           subtitle="Technologies I work with."
@@ -23,7 +24,7 @@ function Skills() {
             </div>
           ))}
         </div>
-      </div>
+      </Container>
     </section>
   );
 }
