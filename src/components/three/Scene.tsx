acce8550653bfca0b5821a8e7cc22441d6910736
PES2UG23CS117(BANGI AMIL JASON ) <153606611+Amiljason@users.noreply.github.com>
2026-07-06 18:11:43 +0000

    Add initial 3D hero background

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -1,5 +1,23 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 function Scene() {
-  return null;
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <ambientLight intensity={1} />
+
+      <mesh>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial wireframe />
+      </mesh>
+
+      <OrbitControls
+        enableZoom={false}
+        autoRotate
+        autoRotateSpeed={1.5}
+      />
+    </Canvas>
+  );
 }
 
 export default Scene;
